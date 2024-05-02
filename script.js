f35671bac1a717d0ad98d7d6a9fe5531a1515a2e
EloiseMcGlasson <mc.glasson@free.fr>
2024-05-02 11:24:17 +0200

    ajout background pour corriger erreur cors

--- script.js
+++ script.js
@@ -1,61 +1,25 @@
 
 console.log("Hello Extension");
 
+
+
+
+
 function getUserSelection() {
   if (window.getSelection) {
-    const selection = window.getSelection().toString();
-    console.log("selection", selection);
+      const selection = window.getSelection().toString();
+      console.log("selection", selection);
 
-    const payload = {
-      text: selection.trim(),
-      src: "English",
-      dest: "minion",
-      detected: "English",
-      
-    };
+      const payload = {
+          text: selection.trim(),
+          src: "English",
+          dest: "minion",
+          detected: "English",
 
-    fetch("https://api.funtranslations.com/translate/minion", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(data => data.json())
-      .then(res => {
-        console.log("res", res);
-        if (res.list && res.list.length > 0) {
-          const translation = res.list[0].destWord;
-          console.log("traduction", translation);
-          insertHtmlAfterSelection(window.getSelection(), translation);
-        }
-      });
-  }
-}
-
-// https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
-function insertHtmlAfterSelection(selectionObject, translation) {
-  let range;
-  let expandedSelRange;
-  
-  if (selectionObject.getRangeAt && selectionObject.rangeCount) {
-    range = selectionObject.getRangeAt(0);
-    expandedSelRange = range.cloneRange();
-    range.collapse(false);
-
-    // Range.createContextualFragment() would be useful here but is
-    // non-standard and not supported in all browsers (IE9, for one)
-    const el = document.createElement("div");
-    el.innerHTML = ` [FR: ${translation} ] `;
-    let frag = document.createDocumentFragment();
-    let node;
-    let lastNode;
-    while ((node = el.firstChild)) {
-      lastNode = frag.appendChild(node);
+      }; 
+      chrome.runtime.sendMessage({ type: 'object', payload }, response => {
+        console.log(response)
+      })
     }
-    range.insertNode(frag);
-    selectionObject.empty();
   }
-}
-
 document.addEventListener("mouseup", getUserSelection);
