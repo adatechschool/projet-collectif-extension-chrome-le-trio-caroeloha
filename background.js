fd512ca45fb254111c0c53534480e90d2f5ea6e3
EloiseMcGlasson <mc.glasson@free.fr>
2024-05-03 15:39:30 +0200

    correction erreur cors

--- background.js
+++ background.js
@@ -1,48 +1,36 @@
-chrome.runtime.onMessage.addListener(function (message, sender, senderResponse) {
-            if (message.type === "object") {
-                console.log(message.payload)
-                fetch('"https://api.funtranslations.com/translate/minion"', {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(message.payload),
-                })
-                    .then(data => data.json())
-                    .then(res => {
-                        console.log("res", res);
-                        if (res.list && res.list.length > 0) {
-                            const translation = res.list[0].destWord;
-                            console.log("traduction", translation);
-                            insertHtmlAfterSelection(window.getSelection(), translation);
-                        }
-                    });
+
+(async () => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "text") {
+
+
+        sendResponse(message.payload.text)
+        fetch('https://api.funtranslations.com/translate/minion?text=' + message.payload.text, {
+            method: 'GET',
+            headers: {
+                "Content-type": "application/json",
+
+
             }
         })
 
-        // https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
-        function insertHtmlAfterSelection(selectionObject, translation) {
-            let range;
-            let expandedSelRange;
-
-            if (selectionObject.getRangeAt && selectionObject.rangeCount) {
-                range = selectionObject.getRangeAt(0);
-                expandedSelRange = range.cloneRange();
-                range.collapse(false);
-
-                // Range.createContextualFragment() would be useful here but is
-                // non-standard and not supported in all browsers (IE9, for one)
-                const el = document.createElement("div");
-                el.innerHTML = ` [FR: ${translation} ] `;
-                let frag = document.createDocumentFragment();
-                let node;
-                let lastNode;
-                while ((node = el.firstChild)) {
-                    lastNode = frag.appendChild(node);
+            .then(data => data.json())
+            .then(res => {
+                
+                console.log("res", res);
+                if (res.list && res.list.length > 0) {
+                    const translation = res.list[0].destWord;
+                    console.log("traduction", translation);
                 }
-                range.insertNode(frag);
-                selectionObject.empty();
-            }
-        }
-  
+            });
+
+        return true
+    }
+})
+})();
+
+
+
+
+
 
--- script.js
+++ script.js
@@ -7,19 +7,49 @@ console.log("Hello Extension");
 
 function getUserSelection() {
   if (window.getSelection) {
-      const selection = window.getSelection().toString();
-      console.log("selection", selection);
+    const selection = window.getSelection().toString();
+    console.log("selection", selection);
 
-      const payload = {
-          text: selection.trim(),
-          src: "English",
-          dest: "minion",
-          detected: "English",
+    const payload = {
+      text: selection.trim(),
+      src: "English",
+      dest: "minion",
+      detected: "English",
 
-      }; 
-      chrome.runtime.sendMessage({ type: 'object', payload }, response => {
-        console.log(response)
-      })
-    }
+    };
+    (async () => {
+      const response = await chrome.runtime.sendMessage({ type: 'text', payload })
+      console.log(response)
+    })();
   }
+
+}
+
 document.addEventListener("mouseup", getUserSelection);
+
+// https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
+function insertHtmlAfterSelection(selectionObject, translation) {
+  let range;
+  let expandedSelRange;
+
+  if (selectionObject.getRangeAt && selectionObject.rangeCount) {
+      range = selectionObject.getRangeAt(0);
+      expandedSelRange = range.cloneRange();
+      range.collapse(false);
+
+      // Range.createContextualFragment() would be useful here but is
+      // non-standard and not supported in all browsers (IE9, for one)
+      const el = document.createElement("div");
+      el.innerHTML = ` [FR: ${translation} ] `;
+      let frag = document.createDocumentFragment();
+      let node;
+      let lastNode;
+      while ((node = el.firstChild)) {
+          lastNode = frag.appendChild(node);
+      }
+      range.insertNode(frag);
+      selectionObject.empty();
+      console.log(translation)
+  }
+}
+insertHtmlAfterSelection(window.getSelection(), translation);
