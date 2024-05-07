9a30399d8f96c5674397e3910f3cfd46b4fc04dd
caro <cirier.c@gmail.com>
2024-05-07 15:49:20 +0200

    ajout des commentaires du code minions

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
-
+// recupere le "sendMessage" du script 
 (async () => {
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.type === "text") {
 
-
-            
-            
+            //appel à l'API + ajoute à la fin de l'URL les paramètres 
             fetch('https://api.funtranslations.com/translate/minion?text=' + message.payload.text, {
                 method: 'GET',
+                //pour contourner l'erreur CORS
                 headers: {
                     "access-control-allow-origin": "*",
                     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
@@ -15,15 +14,18 @@
                 }
             })
 
+                //transformation des données en JSON
                 .then(data => data.json())
                 .then(res => {
+                    //gestion des erreurs d'API
                     if(res.error){
                         sendResponse("banana Error")
                     }else{
-                    
+                    // envoi de la traduction vers script (= le client)
                     sendResponse(res.contents.translated);}
                 });
-
+            
+            //specifique asynchronie    
             return true
         }
     })
--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
 console.log("Hello Extension");
 
-
-
-
-
+// recupere datas et les mets dans la variable selection puis envoyee dans background
 function getUserSelection() {
 
   const selection = window.getSelection().toString();
   if (selection) {
     console.log("selection", selection);
 
+    //paramètres à envoyer à l'api 
     const payload = {
       text: selection.trim(),
       src: "English",
       dest: "minion",
       detected: "English",
-
     };
+
+    //envoi des paramètres au background grâce à sendMessage
     (async () => {
       const response = await chrome.runtime.sendMessage({ type: 'text', payload })
       console.log(response)
@@ -27,29 +26,43 @@ function getUserSelection() {
 
 }
 
+//event de sélection de texte
 document.addEventListener("mouseup", getUserSelection);
 
-// https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
+//affichage sur le navigateur de la réponse de l'API
+// Source du code utilisé : https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
 function insertHtmlAfterSelection(selectionObject, translation) {
   let range;
-  let expandedSelRange;
 
+  //vérifie que les méthodes getRangeAt et rangeCount sont disponibles sur l'objet sélectionné
   if (selectionObject.getRangeAt && selectionObject.rangeCount) {
+      
+      //retourne la range sélectionnée sous forme d'objet
       range = selectionObject.getRangeAt(0);
-      expandedSelRange = range.cloneRange();
+      //place curseur à la fin de la sélection
       range.collapse(false);
 
       // Range.createContextualFragment() would be useful here but is
       // non-standard and not supported in all browsers (IE9, for one)
       const el = document.createElement("div");
-      el.innerHTML = `<span style="color:#8145B5";>[Minion: ${translation} ]</span>`;
+      
+      //place traduction + style à l'intérieur de l'HTML
+      el.innerHTML = `<span style="color:#CC6633";>[ &#x1F601 Minion : ${translation} ]</span>`;
+      
+      //crée un fragment
       let frag = document.createDocumentFragment();
       let node;
       let lastNode;
+      
+      //parcours le texte sélectionné
       while ((node = el.firstChild)) {
+          //node devient enfant de frag
           lastNode = frag.appendChild(node);
       }
+
+      //insérer le frag dans la page
       range.insertNode(frag);
+      //effacer la sélection
       selectionObject.empty();
       
   }
