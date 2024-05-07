ff46138b8b30ea95cbc8056b3b4eea6dc3212171
Hassen <h.hassen.malek@gmail.com>
2024-05-07 15:26:23 +0200

    Ajout des commentaires

--- script.js
+++ script.js
@@ -1,52 +1,70 @@
 
-console.log("Hello Extension");
+console.log("Hello Extension")
 
-
-
-
-
+//c'est une fonction qui permet de selectionner un texte.
 function getUserSelection() {
+
+  //utilisation de la methode getSelection() qui permet de selectionner un texte et le type en string
   const selection = window.getSelection().toString();
   if (selection) {
+
     console.log("selection", selection);
+
+    //appel la fonction encode pour traduire le text selectionné en code morse.
     const translation = encode(selection)
 
+    //appel de la fonction insertHtmlAfterSelection pour afficher le code morse traduit.
     insertHtmlAfterSelection (window.getSelection(), translation);
   }
 }
 
 document.addEventListener("mouseup", getUserSelection);
 
-// https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
+// Source de la fonction insertHtmlAfterSelection: https://stackoverflow.com/questions/3597116/insert-html-after-a-selection
+
 function insertHtmlAfterSelection(selectionObject, translation) {
   let range;
   let expandedSelRange;
 
+  // une condition if qui permet a se positionner a la fin du texte sélectionné.
   if (selectionObject.getRangeAt && selectionObject.rangeCount) {
+    
+    //prend le text ainsi que le html associé
       range = selectionObject.getRangeAt(0);
-      expandedSelRange = range.cloneRange();
+      
+      console.log("test Range", range)
+    //se positionner à la fin du text selectionné
       range.collapse(false);
 
       // Range.createContextualFragment() would be useful here but is
       // non-standard and not supported in all browsers (IE9, for one)
+
+      //création de l'élément div qui contiendra la traduction
       const el = document.createElement("div");
+
+      //affectation du code html + le style + la traduction
       el.innerHTML = `<span style="color: #CC6633;">[Morse: ${translation} ]</span>`;
+      
+      //insertion de l'elelement dans la page
       let frag = document.createDocumentFragment();
+      
       console.log(el)
+      
       let node;
       let lastNode;
+      //parcourir le text selectionné
       while ((node = el.firstChild)) {
+        // le node va devenir l'enfant de frag
           lastNode = frag.appendChild(node);
       }
+      //inserer le fragment dans la plage
       range.insertNode(frag);
+
+      // Effaccer l'ancienne traduction.
       selectionObject.empty();
       console.log(translation)
   }
 }
-
-
-
-
 
 //Code morse
 
@@ -91,52 +109,3 @@ function encode(phrase) {
   return morseCode.trim();
 
 }
-
-// function decode(phrase2) {
-//   let morseToLatin = {
-//       '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
-//       '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
-//       '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z',
-//       '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
-//       '-....': '6', '--...': '7', '---..': '8', '----.': '9',
-//       '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/', '-.--.': '(', '-.--.-': ')',
-//       '.-...': '&', '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_', '.-..-.': '"',
-//       '...-..-': '$', '.--.-.': '@',
-//       '.--.-': 'À', '.-.-': 'Ä', '.-.-': 'Æ', '-.-..': 'Ç', '..-..': 'É', '.-..-': 'È', '---.': 'Ö', '..--': 'Ü',
-//       '...--..': 'ß', '--.--': 'Ñ', '--..-': 'Ź', '/': ' '
-//   };
-
-//   // Diviser le code Morse en caractères séparés
-//   const mots = phrase2.split(' ');
-//   let char = "";
-//   for (let mot of mots) {
-//       if (mot in morseToLatin) {
-//           char += morseToLatin[mot]
-//       } else {
-//           char += " "
-//       }
-//   }
-//   return char;
-// }
-
-// let inputText = document.getElementById("inputText");
-// let Button = document.getElementById("Button");
-// Button.addEventListener("click", function () {
-//   // Récupérer le texte saisi
-//   let phrase = inputText.value;
-//   console.log("Texte saisi :", phrase);
-//   getLatinCharacterList(phrase)
-//   let morse = encode(phrase);
-//   console.log("morse " + morse);
-//   document.getElementById("characterMorse").innerText = " La traduction de " + phrase + " en morse est :  " + morse;
-//});
-
-
-// let inputText2 = document.getElementById("inputText2");
-// let Button2 = document.getElementById("Button2");
-// Button2.addEventListener("click", function () {
-//   let phrase2 = inputText2.value
-//   let latin = decode(phrase2)
-//   console.log("latin " + latin)
-//   document.getElementById("phraseMorse").innerText = " La traduction de " + phrase2 + " en latin est :  " + latin;
-// });
